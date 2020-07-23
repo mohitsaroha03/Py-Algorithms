b5575d5206ad8d6c6e627205ae8b3cfe6bf5ee9d
Mohit Saroha <MohitSaroha@Etechaces.com>
2020-07-23 17:35:57 +0530

    minor

---
+++ src/chapter06trees/BuildTreeWithInorder.py
@@ -0,0 +1,94 @@
+# isGFG: , Link: 
+# IsDone: 0
+
+# Python3 program to construct tree from 
+# inorder traversal 
+
+# Helper class that allocates a new node 
+# with the given data and None left and 
+# right pointers. 
+class newNode: 
+	def __init__(self, data): 
+		self.data = data 
+		self.left = None
+		self.right = None
+
+# Prototypes of a utility function to get 
+# the maximum value in inorder[start..end] 
+
+# Recursive function to construct binary of 
+# size len from Inorder traversal inorder[]. 
+# Initial values of start and end should be 
+# 0 and len -1. 
+def buildTree (inorder, start, end): 
+	if start > end: 
+		return None
+
+	# Find index of the maximum element 
+	# from Binary Tree 
+	i = Max (inorder, start, end) 
+
+	# Pick the maximum value and make it root 
+	root = newNode(inorder[i]) 
+
+	# If this is the only element in 
+	# inorder[start..end], then return it 
+	if start == end: 
+		return root 
+
+	# Using index in Inorder traversal, 
+	# construct left and right subtress 
+	root.left = buildTree (inorder, start, i - 1) 
+	root.right = buildTree (inorder, i + 1, end) 
+
+	return root 
+
+# UTILITY FUNCTIONS 
+
+# Function to find index of the maximum 
+# value in arr[start...end] 
+def Max(arr, strt, end): 
+	i, Max = 0, arr[strt] 
+	maxind = strt 
+	for i in range(strt + 1, end + 1): 
+		if arr[i] > Max: 
+			Max = arr[i] 
+			maxind = i 
+	return maxind 
+
+# This funtcion is here just to test buildTree() 
+def printInorder (node): 
+	if node == None: 
+		return
+
+	# first recur on left child 
+	printInorder (node.left) 
+
+	# then print the data of node 
+	print node.data
+
+	# now recur on right child 
+	printInorder(node.right) 
+
+# Driver Code 
+if __name__ == '__main__': 
+	
+	# Assume that inorder traversal of 
+	# following tree is given 
+	#	 40 
+	# / \ 
+	# 10	 30 
+	# /		 \ 
+	#5		 28 
+
+	inorder = [5, 10, 40, 30, 28] 
+	Len = len(inorder) 
+	root = buildTree(inorder, 0, Len - 1) 
+
+	# Let us test the built tree by 
+	# printing Insorder traversal 
+	print("Inorder traversal of the", 
+			"constructed tree is ") 
+	printInorder(root) 
+	
+# This code is contributed by PranchalK 
--- src/chapter06trees/CeilInBST.py
+++ src/chapter06trees/CeilInBST.py
@@ -1,128 +1,50 @@
-# isGFG: , Link: 
-# IsDone: 0
-import sys
-class BSTNode:
-    def __init__(root, data):
-        root.left = None
-        root.right = None
-        root.data = data
+# isGFG: , Link: https://www.geeksforgeeks.org/floor-and-ceil-from-a-bst/ 
+# IsDone: 1
 
-def insertNode(root, node):
-    if root is None:
-        root = node
-    else:
-        if root.data > node.data:
-            if root.left == None:
-                root.left = node
-            else:
-                insertNode(root.left, node)
-        else:
-            if root.right == None:
-                root.right = node
-            else:
-                insertNode(root.right, node)
+# Python program to find ceil of a given value in BST 
 
-def deleteNode(root, data):
-	""" delete the node with the given data and return the root node of the tree """	    
-	if root.data == data:
-		# found the node we need to delete
-		if root.right and root.left: 
-			# get the successor node and its parent 
-			[psucc, succ] = findMin(root.right, root)
-			# splice out the successor
-			# (we need the parent to do this) 
-			if psucc.left == succ:
-				psucc.left = succ.right
-			else:
-				psucc.right = succ.right					
-			# reset the left and right children of the successor
-			succ.left = root.left
-			succ.right = root.right
-			return succ                
-		else:
-			# "easier" case
-			if root.left:
-				return root.left  # promote the left subtree
-			else:
-				return root.right  # promote the right subtree 
-	else:
-		if root.data > data:  # data should be in the left subtree
-			if root.left:
-				root.left = deleteNode(root.left, data)
-			# else the data is not in the tree 
-		else:  # data should be in the right subtree
-			if root.right:
-				root.right = deleteNode(root.right, data)
-	return root
-
-def findMin(root, parent):
-	""" return the minimum node in the current tree and its parent """
-	# we use an ugly trick: the parent node is passed in as an argument
-	# so that eventually when the leftmost child is reached, the 
-	# call can return both the parent to the successor and the successor
-	if root.left:
-		return findMin(root.left, root)
-	else:
-		return [parent, root]
-
-def inOrderTraversal(root):
-    if not root:
-        return
-    inOrderTraversal(root.left)
-    print root.data
-    inOrderTraversal(root.right)
-
-def preOrderTraversal(root):
-    if not root:
-        return        
-    print root.data
-    preOrderTraversal(root.left)
-    preOrderTraversal(root.right)    
-
-prev = None
-def FloorInBST(root, data):
-	global prev
-	return FloorInBSTUtil(root, data)
-
-def FloorInBSTUtil(root, data):
-	global prev
-	if(not root): 
-		return None
-
-	if(not FloorInBSTUtil(root.left, data)):
-		return 0
-	if(root.data == data) :
-		return root
-
-	if(root.data > data): 	
-		return prev
-
-	prev = root
-	return FloorInBSTUtil(root.right, data)
-
-def CeilInBST(root, data):
-	# Base case
-	if(root == None):
-		return -sys.maxint
-	# We found equal data
-	if(root.data == data):
-		return root.data
-
-	# If root's data is smaller, ceil must be in right subtree
-	if(root.data < data):
-		return CeilInBST(root.right, data)
-	# Else, either left subtree or root has the ceil data
-	ceil = CeilInBST(root.left, data)
-	if ceil >= data:
-		return ceil
-	else: return root.data
+# A Binary tree node 
+class Node: 
 	
-root = BSTNode(3)
-insertNode(root, BSTNode(57))
-insertNode(root, BSTNode(14))
-insertNode(root, BSTNode(35))
-insertNode(root, BSTNode(2))
-insertNode(root, BSTNode(98))
-# inOrderTraversal(root)
-for i  in range(10):
-	print i, "ceil is ", CeilInBST(root, i)
+	# Constructor to create a new node 
+	def __init__(self, data): 
+		self.key = data 
+		self.left = None
+		self.right = None
+
+# Function to find ceil of a given input in BST. If input 
+# is more than the max key in BST, return -1 
+def ceil(root, inp): 
+	
+	# Base Case 
+	if root == None: 
+		return -1
+	
+	# We found equal key 
+	if root.key == inp : 
+		return root.key 
+	
+	# If root's key is smaller, ceil must be in right subtree 
+	if root.key < inp: 
+		return ceil(root.right, inp) 
+	
+	# Else, either left subtre or root has the ceil value 
+	val = ceil(root.left, inp) 
+	return val if val >= inp else root.key 
+
+# Driver program to test above function 
+root = Node(8) 
+
+root.left = Node(4) 
+root.right = Node(12) 
+
+root.left.left = Node(2) 
+root.left.right = Node(6) 
+
+root.right.left = Node(10) 
+root.right.right = Node(14) 
+
+for i in range(16): 
+	print "% d % d" %(i, ceil(root, i)) 
+
+# This code is contributed by Nikhil Kumar Singh(nickzuck_007) 
