778f35080d47a149508726abe70077bb538c02e4
Mohit Saroha <MohitSaroha@Etechaces.com>
2020-09-14 09:35:42 +0530

    stack

--- src/3.4Stacks/InfixToPostfix.py
+++ src/3.4Stacks/InfixToPostfix.py
@@ -1,58 +1,95 @@
-# Link: 
+# Link: https://www.geeksforgeeks.org/stack-set-2-infix-to-postfix/
 # IsDone: 0
-class Stack:
-    def __init__(self):
-        self.items = []
-         
-    # method for pushing an item on a stack
-    def push(self, item):
-        self.items.append(item)
-         
-    # method for popping an item from a stack
-    def pop(self):
-        return self.items.pop()
-     
-    # method to check whether the stack is empty or not
-    def isEmpty(self):
-        return (self.items == [])
-     
-    # method to get the top of the stack
-    def peek(self):
-        return self.items[-1]
-     
-    def __str__(self):
-        return str(self.items)
+# Python program to convert infix expression to postfix 
 
-def infixToPostfix(infixexpr):
-    prec = {}
-    prec["*"] = 3
-    prec["/"] = 3
-    prec["+"] = 2
-    prec["-"] = 2
-    prec["("] = 1
-    opStack = Stack()
-    postfixList = []
-    tokenList = infixexpr.split()
+# Class to convert the expression 
+class Conversion: 
+	
+	# Constructor to initialize the class variables 
+	def __init__(self, capacity): 
+		self.top = -1
+		self.capacity = capacity 
+		# This array is used a stack 
+		self.array = [] 
+		# Precedence setting 
+		self.output = [] 
+		self.precedence = {'+':1, '-':1, '*':2, '/':2, '^':3} 
+	
+	# check if the stack is empty 
+	def isEmpty(self): 
+		return True if self.top == -1 else False
+	
+	# Return the value of the top of the stack 
+	def peek(self): 
+		return self.array[-1] 
+	
+	# Pop the element from the stack 
+	def pop(self): 
+		if not self.isEmpty(): 
+			self.top -= 1
+			return self.array.pop() 
+		else: 
+			return "$"
+	
+	# Push the element to the stack 
+	def push(self, op): 
+		self.top += 1
+		self.array.append(op) 
 
-    for token in tokenList:
-        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
-            postfixList.append(token)
-        elif token == '(':
-            opStack.push(token)
-        elif token == ')':
-            topToken = opStack.pop()
-            while topToken != '(':
-                postfixList.append(topToken)
-                topToken = opStack.pop()
-        else:
-            while (not opStack.isEmpty()) and \
-               (prec[opStack.peek()] >= prec[token]):
-                  postfixList.append(opStack.pop())
-            opStack.push(token)
+	# A utility function to check is the given character 
+	# is operand 
+	def isOperand(self, ch): 
+		return ch.isalpha() 
 
-    while not opStack.isEmpty():
-        postfixList.append(opStack.pop())
-    return " ".join(postfixList)
+	# Check if the precedence of operator is strictly 
+	# less than top of stack or not 
+	def notGreater(self, i): 
+		try: 
+			a = self.precedence[i] 
+			b = self.precedence[self.peek()] 
+			return True if a <= b else False
+		except KeyError: 
+			return False
+			
+	# The main function that converts given infix expression 
+	# to postfix expression 
+	def infixToPostfix(self, exp): 
+		
+		# Iterate over the expression for conversion 
+		for i in exp: 
+			# If the character is an operand, 
+			# add it to output 
+			if self.isOperand(i): 
+				self.output.append(i) 
+			
+			# If the character is an '(', push it to stack 
+			elif i == '(': 
+				self.push(i) 
 
-print(infixToPostfix("A * B + C * D"))
-print(infixToPostfix("( A + B ) * C - ( D - E ) * ( F + G )"))
+			# If the scanned character is an ')', pop and 
+			# output from the stack until and '(' is found 
+			elif i == ')': 
+				while( (not self.isEmpty()) and self.peek() != '('): 
+					a = self.pop() 
+					self.output.append(a) 
+				if (not self.isEmpty() and self.peek() != '('): 
+					return -1
+				else: 
+					self.pop() 
+
+			# An operator is encountered 
+			else: 
+				while(not self.isEmpty() and self.notGreater(i)): 
+					self.output.append(self.pop()) 
+				self.push(i) 
+
+		# pop all the operator from the stack 
+		while not self.isEmpty(): 
+			self.output.append(self.pop()) 
+
+		print "".join(self.output) 
+
+# Driver program to test above function 
+exp = "a+b*(c^d-e)^(f+g*h)-i"
+obj = Conversion(len(exp)) 
+obj.infixToPostfix(exp) 
